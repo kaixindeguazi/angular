9188b6cd01dbfa98a0e3aa8226aad038f6a58ff0
kaixindeguazi <1009641444@qq.com>
2017-06-05 10:55:02 +0800

    Access-Control-Allow-Origin

--- site.php
+++ site.php
@@ -5,7 +5,7 @@
 	//     'http://client1.runoob.com',  
 	//     'http://client2.runoob.com'  
 	// );  
-	  
+
 	// if(in_array($origin, $allow_origin)){  
 	//     header('Access-Control-Allow-Origin:'.$origin);       
 	// } 
