0bdabee10f90ed0d444de0106f9898b0b662be79
kaixindeguazi <1009641444@qq.com>
2017-06-15 18:02:08 +0800

    site

--- site.php
+++ site.php
@@ -1,14 +1,14 @@
 <?php 
 
-	// $origin = isset($_SERVER['HTTP_ORIGIN'])? $_SERVER['HTTP_ORIGIN'] : '';    
-	// $allow_origin = array(  
-	//     'http://client1.runoob.com',  
-	//     'http://client2.runoob.com'  
-	// );  
+	$origin = isset($_SERVER['HTTP_ORIGIN'])? $_SERVER['HTTP_ORIGIN'] : '';    
+	$allow_origin = array(  
+	    'http://client1.runoob.com',  
+	    'http://client2.runoob.com'  
+	);  
 
-	// if(in_array($origin, $allow_origin)){  
-	//     header('Access-Control-Allow-Origin:'.$origin);       
-	// } 
+	if(in_array($origin, $allow_origin)){  
+	    header('Access-Control-Allow-Origin:'.$origin);       
+	} 
 
 	header('Access-Control-Allow-Origin:*');
 	$data = array(
